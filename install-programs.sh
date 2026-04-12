98f7cf674a2317a7229505a8e56b270e031569ed
Juliano Soares da Silva <bytebytebug@gmail.com>
2026-04-11 21:40:25 -0300

    feat: add last minimum deps

--- install-programs.sh
+++ install-programs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+set -e
+
 sudo pacman -S \
+    ttf-dejavu noto-fonts noto-fonts-emoji ttf-liberation \
+    gst-libav gst-plugins-bad gst-plugins-good gst-plugins-ugly ffmpeg gstreamer \
+    hyprland xdg-desktop-portal xdg-desktop-portal-hyprland \
+    kitty \
+    hyprpaper \
     firefox \
     nautilus \
     evince \
-    hyprpaper \
-    code \
     neovim \
-    tmux
+    code \
+    tmux \
+    github-cli \
+    less
 
-yay -S vicinae quickshell
+yay -S vicinae-bin quickshell
--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 git clone https://github.com/bytebytebug/hypr.git ~/.config/hypr
 git clone https://github.com/bytebytebug/quickshell.git ~/.config/quickshell
 
+gsettings set org.gnome.desktop.wm.preferences button-layout ''
+
 
